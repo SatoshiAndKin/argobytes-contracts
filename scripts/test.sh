68df56879f3d1ec063e47f48fe9dac537495bc81
Bryan Stitt <bryan@satoshiandkin.com>
2020-05-21 11:22:45 -0700

    helpful error

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -ex
 
 # TODO" enter the virtualenv and set flags so ganache-cli doesn't OOM
-[ -n "$VIRTUAL_ENV" ]
+if [ -z "$VIRTUAL_ENV" ]; then
+  echo "please 'source scripts/activate' and then try again"
+  exit 9
+fi
 
 # run the tests against the proper network with concurrency
 # TODO: run with -n2 when ganache-cli doesn't have so many issues with OOMing
