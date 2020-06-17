613d4d80346afb1c50ccd48ec07e455453bc3377
Bryan Stitt <bryan@satoshiandkin.com>
2020-06-17 13:33:53 -0700

    it compiles

--- contracts/diamond/DiamondDeployer.sol
+++ contracts/diamond/DiamondDeployer.sol
@@ -3,6 +3,7 @@ pragma solidity 0.6.10;
 pragma experimental ABIEncoderV2;
 
 import {Diamond} from "./Diamond.sol";
+import {IDiamondCutter} from "./DiamondHeaders.sol";
 
 // TODO: cute name like DiamondMine? Carbon?
 contract DiamondDeployer {
@@ -11,16 +12,23 @@ contract DiamondDeployer {
     constructor(
         bytes32 diamond_salt,
         bytes32 cutter_salt,
-        bytes32 loupe_salt
+        bytes32 loupe_salt,
+        bytes[] memory diamond_cuts
     ) public payable {
         Diamond diamond = new Diamond{salt: diamond_salt, value: msg.value}(
             cutter_salt,
             loupe_salt
         );
 
-        // cut functions
+        // add functions
+        bytes memory cut_data = abi.encodeWithSelector(
+            IDiamondCutter.diamondCut.selector,
+            diamond_cuts
+        );
+        (bool success, ) = address(diamond).call(cut_data);
+        require(success, "Adding functions failed.");
 
-        // transfer admin role to msg.sender
+        // transfer admin role from `this` to `msg.sender`
         bytes32 admin_role = diamond.DEFAULT_ADMIN_ROLE();
 
         diamond.grantRole(admin_role, msg.sender);
