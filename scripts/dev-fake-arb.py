87d42ecad0dfdf83a8ab09bf8c3e83759100606e
Bryan Stitt <bryan@satoshiandkin.com>
2020-06-16 22:37:39 -0700

    start adding helper script

---
+++ scripts/dev-fake-arb.py
@@ -0,0 +1,32 @@
+from brownie import *
+
+
+def reset_block_time(synthetix_depot_action):
+    # synthetix_address_resolver = interface.IAddressResolver(SynthetixAddressResolver)
+
+    # TODO: get this from the address resolver instead
+    synthetix_exchange_rates = Contract.from_explorer("0x9D7F70AF5DF5D5CC79780032d47a34615D1F1d77")
+
+    token_bytestr = synthetix_depot_action.BYTESTR_ETH()
+
+    last_update_time = synthetix_exchange_rates.lastRateUpdateTimes(token_bytestr)
+
+    print("last_update_time: ", last_update_time)
+
+    assert last_update_time != 0
+
+    latest_block_time = web3.eth.getBlock(web3.eth.blockNumber).timestamp
+
+    print("latest_block_time:", latest_block_time)
+
+    assert latest_block_time != 0
+
+    web3.testing.mine(last_update_time)
+
+
+def main():
+    synthetix_depot_action = None  # TODO: how should we get this?
+
+    reset_block_time(synthetix_depot_action)
+
+    # TODO: put some ETH onto one of the actions to simulate an arbitrage gains
