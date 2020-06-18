6144c3da019953d7b834f2efcc97dc05a57f99b2
Bryan Stitt <bryan@satoshiandkin.com>
2020-06-17 17:09:38 -0700

    gastoken should be an interface, not an abstract

--- contracts/interfaces/gastoken/IGasToken.sol
+++ contracts/interfaces/gastoken/IGasToken.sol
@@ -1,27 +1,24 @@
 // SPDX-License-Identifier: You can't license an interface
 pragma solidity 0.6.10;
 
-abstract contract IGasToken {
-    function free(uint256 value) public virtual returns (bool success);
+interface IGasToken {
+    function free(uint256 value) external returns (bool success);
 
-    function freeUpTo(uint256 value) public virtual returns (uint256 freed);
+    function freeUpTo(uint256 value) external returns (uint256 freed);
 
     function freeFrom(address from, uint256 value)
-        public
-        virtual
+        external
         returns (bool success);
 
     function freeFromUpTo(address from, uint256 value)
-        public
-        virtual
+        external
         returns (uint256 freed);
 
-    function mint(uint256 value) public virtual;
+    function mint(uint256 value) external;
 
     function approve(address spender, uint256 value)
-        public
-        virtual
+        external
         returns (bool success);
 
-    function balanceOf(address owner) public virtual returns (uint256 balance);
+    function balanceOf(address owner) external returns (uint256 balance);
 }
