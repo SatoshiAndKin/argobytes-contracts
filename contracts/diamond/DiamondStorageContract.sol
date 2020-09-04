630857aa5bda00dbdd76bddf38bd043662ce7c13
Bryan Stitt <bryan@satoshiandkin.com>
2020-09-04 15:26:26 -0700

    update part of the diamond standard
    there have been a bunch more changes that we should merge in

--- contracts/diamond/DiamondStorageContract.sol
+++ contracts/diamond/DiamondStorageContract.sol
@@ -4,33 +4,39 @@ pragma solidity 0.7.0;
 import {AccessControl} from "@OpenZeppelin/access/AccessControl.sol";
 
 contract DiamondStorageContract is AccessControl {
+
+    bytes32 constant DIAMOND_STORAGE_POSITION = keccak256("diamond.standard.diamond.storage");
+
     struct DiamondStorage {
+
+        // the reference implementation uses a single address for the contract owner. this could do more complex things if it were a smart contract,
+        // but I think for now we'll keep using OpenZepplin's AccessControl
+        // owner of the contract
+        // address contractOwner;
+
         // maps function selectors to the facets that execute the functions.
         // and maps the selectors to the slot in the selectorSlots array.
         // and maps the selectors to the position in the slot.
-        // func selector => address facet, uint64 slotsIndex, uint64 slotIndex
+        // func selector => address facet, uint32 slotIndex, uint64 slotsIndex
         mapping(bytes4 => bytes32) facets;
+
         // array of slots of function selectors.
         // each slot holds 8 function selectors.
-        mapping(uint256 => bytes32) selectorSlots;
-        // uint128 numSelectorsInSlot, uint128 selectorSlotsLength
+        mapping(uint => bytes32) selectorSlots;
+
+        // uint32 selectorSlotLength, uint32 selectorSlotsLength
         // selectorSlotsLength is the number of 32-byte slots in selectorSlots.
         // selectorSlotLength is the number of selectors in the last slot of
         // selectorSlots.
-        uint256 selectorSlotsLength;
+        uint selectorSlotsLength;
+
         // Used to query if a contract implements an interface.
         // Used to implement ERC-165.
         mapping(bytes4 => bool) supportedInterfaces;
     }
 
-    function diamondStorage()
-        internal
-        pure
-        returns (DiamondStorage storage ds)
-    {
-        // ds_slot = keccak256("diamond.standard.diamond.storage");
-        assembly {
-            ds.slot := 0xc8fcad8db84d3cc18b4c41d551ea0ee66dd599cde068d998e57d5e09332c131c
-        }
+    function diamondStorage() internal pure returns(DiamondStorage storage ds) {
+        bytes32 position = DIAMOND_STORAGE_POSITION;
+        assembly { ds.slot := position }
     }
 }
