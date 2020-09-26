5cdf399f39340328a2190cdf7fb94031af570c75
Bryan Stitt <bryan@satoshiandkin.com>
2020-09-25 18:12:00 -0700

    test more

--- tests/test_argobytes_proxy.py
+++ tests/test_argobytes_proxy.py
@@ -24,14 +24,14 @@ def test_argobytes_arbitrage_access_control(address_zero, argobytes_actor, argob
         borrows, argobytes_actor, actions,
     )
 
-    # # check that accounts[0] is allowed
-    # argobytes_proxy.execute(
-    #     False,
-    #     False,
-    #     argobytes_trader.address,
-    #     argobytes_trader_calldata,
-    #     {"from": accounts[0], "value": value}
-    # )
+    # check that accounts[0] is allowed
+    argobytes_proxy.execute(
+        False,
+        False,
+        argobytes_trader.address,
+        argobytes_trader_calldata,
+        {"from": accounts[0]}
+    )
 
     # check that accounts[1] is NOT allowed
     with brownie.reverts("ArgobytesAuth: 403"):
@@ -40,11 +40,14 @@ def test_argobytes_arbitrage_access_control(address_zero, argobytes_actor, argob
             False,
             argobytes_trader.address,
             argobytes_trader_calldata,
-            {"from": accounts[1], "value": value}
+            {"from": accounts[1]}
         )
 
     # TODO: approve accounts[1]
+    assert False
+
     # TODO: check that accounts[1] is allowed
+    assert False
 
 
 def test_simple_sweep(address_zero, argobytes_actor, argobytes_trader, argobytes_proxy, example_action, kollateral_invoker):
@@ -126,25 +129,24 @@ def test_liquidgastoken_saves_gas(address_zero, argobytes_actor, argobytes_trade
     value = 1e18
     gas_price = 150 * 1e9  # 150 gwei
 
-    assert argobytes_proxy.balance() == 0
-    assert example_action.balance() == 0
-
     # send some ETH into the sweep contract to simulate arbitrage profits
-    accounts[0].transfer(example_action, value)
+    accounts[1].transfer(example_action, value)
 
     starting_balance = accounts[0].balance()
 
     # make sure balances match what we expect
     assert starting_balance > value
+    assert argobytes_actor.balance() == 0
+    assert argobytes_proxy.balance() == 0
+    assert argobytes_trader.balance() == 0
     assert example_action.balance() == value
 
     borrows = []
-
-    # sweep and use up gas
     actions = [
+        # sweep and use up gas
         (
             example_action,
-            example_action.sweep.encode_input(address_zero, address_zero, 100000),
+            example_action.sweep.encode_input(address_zero, address_zero, 1000000),
             True,
         ),
     ]
@@ -156,18 +158,20 @@ def test_liquidgastoken_saves_gas(address_zero, argobytes_actor, argobytes_trade
         argobytes_trader.address,
         argobytes_trader.atomicArbitrage.encode_input(
             borrows, argobytes_actor, actions
-        ),
-        {
-            # 'gas_price': gas_price,
-            "value": value,
-        }
+        )
     )
 
     profit = decode_single('uint256', arbitrage_tx.return_value)
 
     # make sure balances match what we expect
-    assert profit == value
+    assert argobytes_actor.balance() == 0
     assert argobytes_proxy.balance() == 0
+    assert argobytes_trader.balance() == 0
+    assert example_action.balance() == 0
+
+    # TODO: profit is 0 because we only check profit for borrows
+    assert profit == 0
+
     assert accounts[0].balance() == starting_balance + value
 
     gas_used_without_gastoken = arbitrage_tx.gas_used
@@ -175,15 +179,18 @@ def test_liquidgastoken_saves_gas(address_zero, argobytes_actor, argobytes_trade
     print("gas_used_without_gastoken: ", gas_used_without_gastoken)
 
     # move the ETH arb return back to the sweep contract
-    argobytes_proxy.adminCall(address_zero, example_action, to_bytes(hexstr="0x"), value, {'from': accounts[0]})
+    accounts[0].transfer(example_action, value)
 
-    assert argobytes_proxy.balance() == value
+    assert accounts[0].balance() == starting_balance
     assert example_action.balance() == value
 
     # mint and approve some gas token
     # TODO: how much should we make?
-    liquidgastoken.mint(100)
-    liquidgastoken.approve(argobytes_proxy, 100)
+    liquidgastoken.mint(100, {"from": accounts[0]})
+    liquidgastoken.approve(argobytes_proxy, 100, {"from": accounts[0]})
+
+    # minting gas token takes eth. so save our balance again
+    starting_balance = accounts[0].balance()
 
     # do the faked arbitrage trade again (but this time with gas tokens)
     arbitrage_tx = argobytes_proxy.execute(
@@ -197,7 +204,6 @@ def test_liquidgastoken_saves_gas(address_zero, argobytes_actor, argobytes_trade
         ),
         {
             'gas_price': gas_price,
-            'value': value,
         }
     )
 
@@ -209,9 +215,17 @@ def test_liquidgastoken_saves_gas(address_zero, argobytes_actor, argobytes_trade
 
     # TODO: figure out the cost of gas tokens
     # TODO: value should actually be value - cost of gastokens that we bought and freed
-    assert profit > value * 0.9995
-    # assert argobytes_proxy.balance() == 2 * value
-    assert argobytes_proxy.balance() > 1.9995 * value
+    assert argobytes_actor.balance() == 0
+    assert argobytes_proxy.balance() == 0
+    assert argobytes_trader.balance() == 0
+    assert example_action.balance() == 0
+
+    # TODO: profit is 0 because we only check profit for borrows
+    assert profit == 0
+
+    # TODO: check a more specifiic balance
+    assert accounts[0].balance() > starting_balance
+
     assert gas_used_with_gastoken < gas_used_without_gastoken
     # TODO: checking the gas used isn't enough. we need to check that the overall gas cost was less, too
     # TODO: assert something about the number of freed gas tokens. i don't think we are getting optimal amounts
