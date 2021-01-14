cf517f6b0b05075eadd50d4f5d65dfbf288408e6
Bryan Stitt <bryan@satoshiandkin.com>
2021-01-13 19:36:31 -0800

    write UniswapV2Action

--- contracts/actions/UniswapV2Action.sol
+++ contracts/actions/UniswapV2Action.sol
@@ -4,13 +4,85 @@ pragma experimental ABIEncoderV2;
 
 import {IERC20} from "@OpenZeppelin/token/ERC20/IERC20.sol";
 
-import {AbstractERC20Exchange} from "./AbstractERC20Exchange.sol";
 import {
-    IUniswapV2Router01
-} from "contracts/interfaces/uniswap/IUniswapV2Router01.sol";
-import {IUniswapV2Pair} from "contracts/interfaces/uniswap/IUniswapV2Pair.sol";
+    IUniswapV2Router02
+} from "contracts/interfaces/uniswap/IUniswapV2Router02.sol";
+
+import {AbstractERC20Exchange} from "./AbstractERC20Exchange.sol";
 
 contract UniswapV2Action is AbstractERC20Exchange {
-    // TODO: trading
-    // TODO: flash loans
+
+    /*
+    // we don't need this. just use router.swapExactETHForTokens as your action
+    function tradeEtherToToken(
+        address to,
+        address router,
+        address[] calldata path,
+        uint256 dest_min_tokens
+    ) external payable {
+        // function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        //     external
+        //     payable
+        //     returns (uint[] memory amounts);
+        // solium-disable-next-line security/no-block-members
+        IUniswapV2Router02(router).swapExactETHForTokens{
+            value: address(this).balance
+        }(dest_min_tokens, path, to, block.timestamp);
+    }
+    */
+
+    function tradeTokenToToken(
+        address to,
+        address router,
+        address[] calldata path,
+        uint256 dest_min_tokens
+    ) external {
+        uint256 src_balance = IERC20(path[0]).balanceOf(address(this));
+
+        // some contracts do all sorts of fancy approve from 0 checks to avoid front running issues. I really don't see the benefit here
+        IERC20(path[0]).approve(router, src_balance);
+
+        /*
+        function swapExactTokensForTokens(
+            uint amountIn,
+            uint amountOutMin,
+            address[] calldata path,
+            address to,
+            uint deadline
+        ) external returns (uint[] memory amounts);
+        */
+        // solium-disable-next-line security/no-block-members
+        IUniswapV2Router02(router).swapExactTokensForTokens(
+            src_balance,
+            dest_min_tokens,
+            path,
+            to,
+            block.timestamp
+        );
+    }
+
+    function tradeTokenToEther(
+        address payable to,
+        address router,
+        address[] calldata path,
+        uint256 dest_min_tokens
+    ) external {
+        uint256 src_balance = IERC20(path[0]).balanceOf(address(this));
+
+        IERC20(path[0]).approve(router, src_balance);
+
+        /*
+        function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+            external
+            returns (uint[] memory amounts);
+        */
+        // solium-disable-next-line security/no-block-members
+        IUniswapV2Router02(router).swapExactTokensForETH(
+            src_balance,
+            dest_min_tokens,
+            path,
+            to,
+            block.timestamp
+        );
+    }
 }
