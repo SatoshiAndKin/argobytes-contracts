add8032cf58d3be7a83e5c9b08381e31e559cdb7
Bryan Stitt <bryan@stitthappens.com>
2021-08-31 23:44:42 -0700

    more scopes

--- argobytes/argobytes_brownie_project/contracts/actions/leverage/leverageAave/LeverageAaveAction.sol
+++ argobytes/argobytes_brownie_project/contracts/actions/leverage/leverageAave/LeverageAaveAction.sol
@@ -20,12 +20,12 @@ contract LeverageAaveAction is ArgobytesTips {
         uint256 collateral_flash_fee;
         IERC20 borrow;
         uint256 borrow_amount;
-        address swap_contract;
+        address swap_contract;  // a standard exchange. tokens are approved for here
         bytes swap_data;
     }
 
     function enter(EnterData calldata data) external {
-        // at this point, we have some tokens from the flash loan
+        // at this point, we have some collateral tokens from the flash loan
         uint256 flash_collateral_amount = data.collateral.balanceOf(address(this));
         require(flash_collateral_amount > 0, "no flash collateral");
 
@@ -84,13 +84,12 @@ contract LeverageAaveAction is ArgobytesTips {
         uint256 collateral_atoken_tip;
         uint256 collateral_withdraw_amount;
         uint256 borrow_flash_fee;
-        uint256 collateral_swap_amount;
-        address swap_contract;
+        address swap_contract;  // a standard exchange. tokens are approved for here
         bytes swap_data;
     }
 
     function exit(ExitData calldata data) external {
-        // at this point, we have some tokens from the flash loan
+        // at this point, we have some borrow tokens from the flash loan
         uint256 flash_borrow_amount = data.borrow.balanceOf(address(this));
 
         // repay the loan with the flash loaned tokens
@@ -98,14 +97,24 @@ contract LeverageAaveAction is ArgobytesTips {
         data.lending_pool.repay(address(data.borrow), flash_borrow_amount, 2, data.on_behalf_of);
 
         // Transfer aToken here
-        // TODO: calculate collateral_atoken_amount based on what we repaid?
         data.collateral_atoken.transferFrom(data.on_behalf_of, address(this), data.collateral_atoken_amount);
 
-        // optionally tip atokens
-        tip_erc20(data.collateral_atoken, data.collateral_atoken_tip);
+        { // scope for collateral_atoken_amount and collateral_atoken_tip
+            uint collateral_atoken_amount = data.collateral_atoken_amount;
+            uint collateral_atoken_tip = data.collateral_atoken_tip;
 
-        // Burn aToken for the underlying
-        data.collateral_atoken.safeApprove(address(data.lending_pool), data.collateral_withdraw_amount);
+            // optionally tip atokens
+            if (collateral_atoken_tip > 0) {
+                tip_erc20(data.collateral_atoken, collateral_atoken_tip);
+                collateral_atoken_amount -= collateral_atoken_tip;
+            }
+
+            // appove burning aToken for the underlying
+            data.collateral_atoken.safeApprove(address(data.lending_pool), collateral_atoken_amount);
+        }
+        // burning aToken for the underlying
+        // this does not have to be all the atoken!
+        // TODO: option to withdraw all of the atokens? 
         data.lending_pool.withdraw(address(data.collateral), data.collateral_withdraw_amount, address(this));
 
         // we need to pay back the flash loan
@@ -125,21 +134,12 @@ contract LeverageAaveAction is ArgobytesTips {
             balance >= flash_borrow_amount,
             "!swap balance"
         );
-        // check if we have any extra borrow tokens
-        if (balance > flash_borrow_amount) {
-            data.borrow.safeTransfer(data.on_behalf_of, balance - flash_borrow_amount);
-        }
 
-        // check if we have any extra collateral tokens
-        balance = data.collateral.balanceOf(address(this));
-        if (balance > 0) {
-            data.collateral.safeTransfer(data.on_behalf_of, balance);
-        }
-
-        // check if we have any leftover a tokens
+        // check if we have any leftover atokens
         balance = data.collateral_atoken.balanceOf(address(this));
-        if (balance > 0) {
-            data.collateral_atoken.safeTransfer(data.on_behalf_of, balance);
+        if (balance > 1) {
+            // TODO: do we actually want to leave 1 behind for gas efficiency?
+            data.collateral_atoken.safeTransfer(data.on_behalf_of, balance - 1);
         }
 
         // flash_borrow_amount will be pulled by the flash lender
