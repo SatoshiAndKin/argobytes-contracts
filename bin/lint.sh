94f620b29d68b0bf1a3efd39e9d4b03f5891f42d
Bryan Stitt <bryan@stitthappens.com>
2021-09-04 19:10:11 -0700

    lint

--- bin/lint.sh
+++ bin/lint.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 set -eux
 
+# if requirements.in and requirements.txt have changed, run pip-compile
+# TODO: do this in a seperate lint script
+sum_file=.requirements.shasum
+if [ -e "$sum_file" ]; then
+    if sha256sum --check "$sum_file"; then
+        # shasum succeeded. no need to run pip-compile
+        :
+    else
+        # shasum failed! this means one or more of the requirements files has changed
+        pip-compile requirements.in
+    fi
+fi
+
+# make sure shasums match the files that we expect
+echo "# automatically generated file" > "$sum_file"
+sha256sum requirements.in requirements.txt >> "$sum_file"
+
 isort .
 black --line-length 120 argobytes bin setup.py tests
 
